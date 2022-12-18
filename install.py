ad28108a83d9d8c61b984e172ec92afa89f6c2e9
salamandercrossing <giulio.zani@gmail.com>
2022-12-18 13:14:56 +0000

    update

--- install.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.system("python rm -r dist")
-os.system("python setup.py sdist")
-
-file = os.listdir("dist")[0]
-
-os.system(f"pip install dist/{file} --force-reinstall")
--- setup.py
+++ setup.py
@@ -1,28 +1,22 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="yerbamate",
-    description="A cowsay clone for python in one file.",
-    author="Giulio Zani, Ali Rahimi",
-    author_email="yerba.mate.dl@proton.me",
-    url="https://github.com/ilex-paraguariensis/yerbamate",
-    python_requires=">=3.9",
-    version="0.8.12",
+    description="📦 Modularize your deep learning project to improve reproducibility",
+    author="Giulio Zani",
+    author_email="giulio.zani@gmail.com",
+    url="https://github.com/SalamanderXing/mate",
+    python_requires=">=3.10",
+    version="0.9",
     packages=find_packages("packages", exclude=["tests"]),
     include_package_data=True,
     package_dir={"": "packages/"},
     license="Apache License 2.0",
     license_files=("LICENSE.md",),
-    install_requires=[
-        "bomba",
-        "flask",
-        "ipdb",
-        "websockets",
-        "flask_cors",
-        "pipreqs",
-        "dirhash",
-        "docstring-parser",
-    ],
+    install_requires=requirements,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
